0d1be1b24f46e9f802860c8636ffaa4aa8083e37
denysboiko <denys.bojko@gmail.com>
2014-12-02 19:19:01 +0200

    improved templating, grunt, markup

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,10 @@
-function SelectOption() {
+/*
+SelectOption() {
     document.getElementById('dd').firstChild.nodeValue=this.firstChild.nodeValue
 }
 
-$('#workspace').load(function () {
+$('#workspace').on('load',function () {
     var opt = document.getElementById('dd').getElementsByTagName('A')
     for (var i=0; i<opt.length; i++)
         opt[i].onclick=SelectOption
-});
+});*/
